c117eef271dcbd4fdebdfc877ea553fae9c4a280
Zeb Pirzada <zpirzada@gmail.com>
2022-12-29 22:10:48 +0100

    Change request 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+    static void myMsg(){
+        System.out.println(" Hello form the method");
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        myMsg();
     }
 }
